941b216fb4897127af4254895b8288c9318c4afb
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-05-30 13:08:13 +0000

    installed router dom , created routes on header and adjusted styling as needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.Module.css';
+import styles from './App.Module.css';
 import Header from '../src/components/Header'
 import './index.css';
+import Container from 'react-bootstrap/Container'
+import {Route,Switch} from 'react-router-dom'
 
 function App() {
   return (
+    <div className={styles.App}>
     <Header />
+    <Container className={styles.Main}>
+      <Switch>
+        <Route exact path="/Log-in" render= {()=><h1>Login Form</h1>} />
+        <Route exact path="/sign-up" render= {()=><h1>SignUp Form</h1>} />
+        <Route render= {()=><p>Page Not Found!</p>} />
+      </Switch>
+    </Container>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import styles from '../css/Header.Module.css';
 import profilepic from '../assets/defaultprofilepic/p5-default-profile-pic.jpg'
+import {NavLink} from 'react-router-dom'
 
 export default function Header() {
   return (
@@ -11,15 +12,21 @@ export default function Header() {
           <p className={`${styles.whiteText} ${styles.headerSize} me-2`}>Task Pilot</p>
         </div>
         <Nav>
-          <div className={`me-2 ${styles.circularButton}`}>
-            <img className={styles.circularButtonImg} src={profilepic} alt="User Profile Pic" />
-          </div>
+          <NavLink to="/user-profile/${userID}`" className="nav-link">
+            <div className={`me-2 ${styles.circularButton}`}>
+              <img className={styles.circularButtonImg} src={profilepic} alt="User Profile Pic" />
+            </div>
+          </NavLink>
         </Nav>
       </Navbar>
       <div className={styles.sidebar}>
-        <Nav className={styles.navStyling}>
-          <Button variant="outline-light" className="mb-2 ms-1 me-1">Home</Button>
+        <Nav className={styles.navStyling} >
+          <NavLink to="/related-tasks/${userID}`" className={styles.NavLink} activeClassName={styles.Active} >
+            <Button variant="outline-light" className="mb-2 ms-1 me-1">Home</Button>
+          </NavLink>
+          <NavLink to="/inbox/${userID}`" className={styles.NavLink} activeClassName={styles.Active}>
           <Button variant="outline-light" className="mb-2 ms-1 me-1">Inbox</Button>
+          </NavLink>
           <Button variant="outline-light" className={`mb-2 ms-1 me-1 ${styles.signOutButton} ${styles.signOutButtonPositioning}`}>SignOut</Button>
         </Nav>
       </div>
