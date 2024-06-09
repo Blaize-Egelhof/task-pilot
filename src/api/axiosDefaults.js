1cd57638901014ff15e4f91d9026ca56e89dd12f
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-06-09 12:28:37 +0000

    created axios logic to refresh access tokens automatically

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://task-pilot-api-323c9bc2bc87.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
