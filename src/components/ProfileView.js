b0e4cbf78713b069f30227d604976ba8e2432375
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-07-02 12:32:39 +0000

    feat! created styling and media queries for ProfileView component

--- src/components/ProfileView.js
+++ src/components/ProfileView.js
@@ -2,41 +2,40 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Image, Row, Spinner } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import styles from '../css/CreateEditForm.Module.css'
 
 function ProfileView() {
-
+    // State to store profile data
     const [profileData,setProfileData] = useState('');
+    // State to store any errors
     const [errors,setErrors] = useState('');
+    // Extract 'id' parameter from the URL
     const {id} = useParams();
-    const currentUser = useCurrentUser()
+    // Hook for navigating
     const history = useHistory()
-
-    const {bios,created_at,image,is_owner,joined_tasks_count,owner,public_tasks_count} =profileData
-
-    console.log(created_at)
+    // Destructure profile data fields for easy access
+    const {bios,created_at,image,is_owner,joined_tasks_count,owner,owned_tasks_count} =profileData
 
     useEffect(() => {
         const fetchProfileDetails = async () => {
             try {
                const response = await axios.get(`profiles/${id}`)
                setProfileData(response.data)
-               console.log(profileData)
             } catch (err) {
                 setErrors(err.message)
-                console.log(errors)
             }
         }
-
+        // Call the function to fetch profile details
         fetchProfileDetails(); 
 
     }, [id]);
-
+    // Handler for editing the profile
     const handleEditprofile = ()=>{
+        // Navigate to the edit profile page
         history.push(`/edit-profile/${id}`)
     }
-
-    if (!profileData && !errors.fetch) {
+    // Display a spinner while data is being fetched
+    if (!profileData && !errors) {
         return (
           <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
             <Spinner animation="border" variant="primary" />
@@ -44,10 +43,11 @@ function ProfileView() {
         );
       }
     return(<>
-        <Container fluid className='align-items-right'>
-        
+    <Row className={`${styles.profileRow}`}>
+        <Container fluid >
+            {/* Checks if the is_owner variable is True, if so, renders an edit button for profile owners to edit their profile */}
         {is_owner === true && (
-                    <Col lg={2} className="text-right">
+                    <Col lg={2} className="text-right mt-1">
                         <Button onClick={()=>{handleEditprofile()}} variant="primary">Edit</Button>
                     </Col>
                 )}
@@ -55,32 +55,36 @@ function ProfileView() {
         </Container>
         <Container fluid>
             <Row className="align-items-center">
+                {/* Renders the user profile pic in circle format */}
                 <Col lg={4} className="text-center">
-                    <Image src={image} roundedCircle width={150} height={150} className="me-3" />
+                    <Image src={image} roundedCircle width={230} height={230} className="me-3 mt-1" />
                 </Col>
                 <Col lg={8} className="d-flex flex-column justify-content-center">
-                    <div className="mb-2"><strong><h2>{owner}</h2></strong></div>
-                    <div className="text-muted">{`Date Joined : ${created_at}`}</div>
+                    <div className="mb-2 fw-bolder"><strong><h2>{owner}</h2></strong></div>
+                    <div className="text-muted fw-bolder">{`Date Joined : ${created_at}`}</div>
                     <div className="mb-2 mt-5">{bios}</div>
                 </Col>
             </Row>
         </Container>
-        <Container fluid className='mt-5 bg-secondary'>
+        <Container fluid className='mt-2'>
                 <Row className="justify-content-center text-center">
+                    {/* Shows the number of tasks the profile owner has joined*/}
                     <Col lg={3}>
                         <div>
-                            <h4>Joined Tasks</h4>
-                            <div>{joined_tasks_count}</div>
+                            <h4 className={`${styles.HeaderV1}`}>Joined Tasks</h4>
+                            <div className='fw-bolder fs-4'>{joined_tasks_count}</div>
                         </div>
                     </Col>
+                    {/* Shows the number of tasks the profile owner has created*/}
                     <Col lg={3}>
                         <div>
-                            <h4>Public Tasks</h4>
-                            <div>{public_tasks_count}</div>
+                            <h4 className={`${styles.HeaderV1}`}>Owned Tasks</h4>
+                            <div className='fw-bolder fs-4'>{owned_tasks_count}</div>
                         </div>
                     </Col>
                 </Row>
         </Container>
+    </Row>
     </>)
 }
 
