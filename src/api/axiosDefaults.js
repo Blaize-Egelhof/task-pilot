5b51099a8f8abe9f4b45c7a354eafeba0388fef6
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-08-20 10:47:22 +0000

    updated manifest file with application name

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 // Base URL and default headers
-
 axios.defaults.baseURL = 'https://task-pilot-api-323c9bc2bc87.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
